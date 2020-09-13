b3df2efd824e068f2baba8965899229f8c5644d5
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-09-13 11:25:50 +0200

    Favourite Crypto feature -> you can now add or remove cryptos from your favorite list

---
+++ Crypto/Assets/Scripts/Favourites.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Favourites : MonoBehaviour
+{
+
+    #region Singleton
+    private static Favourites instance;
+    public static Favourites Instance
+    {
+        get
+        {
+            if (instance == null) { instance = FindObjectOfType(typeof(Favourites)) as Favourites; }
+            return instance;
+        }
+        set { instance = value; }
+    }
+    #endregion
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        star_image = GetComponent<Image>();
+    }
+
+    public bool isFavourite;
+    public string ID;
+    private Image star_image;
+
+    public void  ADD_TO_FAVOURITES()
+    {
+        ID = transform.parent.transform.parent.name;
+
+        isFavourite = false;
+
+        foreach (string crypto in AppHandler.Instance.FAVOURITE_CRYPTOS)
+        {
+            if (crypto.Equals(ID))
+            {
+                isFavourite = true;
+                break;
+            }
+            else { continue; }
+        }
+
+        if (isFavourite)
+        {
+            // remove from list
+            AppHandler.Instance.FAVOURITE_CRYPTOS.Remove(ID);
+            // change sprite img
+            star_image.sprite = AppHandler.Instance.emptyStar;
+        }
+        else
+        {
+            // add to list
+            AppHandler.Instance.FAVOURITE_CRYPTOS.Add(ID);
+            // change sprite img
+            star_image.sprite = AppHandler.Instance.filledStar;
+        }
+    }
+
+  
+}
