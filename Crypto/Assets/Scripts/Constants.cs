9a08bf8d6e986f9be1428b23e41fb97298c4d0f4
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-09-22 19:24:32 +0200

    AuthController update (exception handling; error throwing) + Constants.cs + Scene Modifications

---
+++ Crypto/Assets/Scripts/Constants.cs
@@ -0,0 +1,22 @@
+﻿public class Constants
+{
+    // ENG
+        // ERROR MESSAGES
+        public const string ERROR_REGISTER_EMAIL            = "Please enter your email address.";
+        public const string ERROR_REGISTER_PASSWORD         = "Please enter your password.";
+        public const string ERROR_REGISTER_CONFIRM_PASSWORD = "Password does not match.";
+        public const string ERROR_REGISTER_TOGGLE           = "Please agree with terms & conditions.";
+
+        public const string ERROR_LOGIN_EMAIL               = "Please enter your email address.";
+        public const string ERROR_LOGIN_PASSWORD            = "Please enter your password.";
+        //
+    //
+
+    // GER
+
+    //
+
+    // RUS
+
+    //
+}
--- Crypto/Assets/Scripts/FireBase/AuthController.cs
+++ Crypto/Assets/Scripts/FireBase/AuthController.cs
@@ -6,31 +6,97 @@ using Firebase.Auth;
 
 public class AuthController : MonoBehaviour
 {
-    [SerializeField] private Text emailInput, passwordInput;
+    /// <summary>
+    /// email & pass for register
+    /// </summary>
+    public InputField emailInput, passwordInput;
+
+    public InputField confirm_passwordInput;
+
+    public Toggle agreement1;
+    public Toggle agreement2;
+
+    public Text ErrorRegisterMessage_Email;
+    public Text ErrorRegisterMessage_Password;
+    public Text ErrorRegisterMessage_PasswordConfirmation;
+    public Text ErrorRegisterMessage_ErrorMsgTerms;
+
+
+    public Button SIGN_IN_BTN;
+    public Button SIGN_UP_BTN;
+
+    // <summary>
+    // email & pass for logging
+    // </summary>
+    [SerializeField]
+    private Text log_in_emailInput, log_in_passwordInput;
+
+    public Text ErrorLoginMessage_Email;
+    public Text ErrorLoginMessage_Password;
+
+
+    public Animator RegistrationCompletedTemplateAnime;
+    public Animator LoginCompletedTemplateAnime;
+
+
+    private DataBridge bridge;
+
+
+    void Awake()
+    {
+        bridge = GetComponent<DataBridge>();
+    }
+
+    void Start()
+    {
+        CleanUpLoginInputs();
+        CleanUpRegisterInputs();
+    }
+
 
     public void SignIn()
     {
+        if ((log_in_emailInput.text == ""))
+        {
+            print("Please enter both email to continue..");
+            ErrorLoginMessage_Email.text = Constants.ERROR_LOGIN_EMAIL;
+            return;
+        }
+        if ((log_in_passwordInput.text == ""))
+        {
+            print("Please enter password to continue..");
+            ErrorLoginMessage_Password.text = Constants.ERROR_LOGIN_PASSWORD;
+            return;
+        }
+
         FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync
-            (emailInput.text, passwordInput.text).ContinueWith((task => {
+            (log_in_emailInput.text, log_in_passwordInput.text).ContinueWith((task => {
 
                 if (task.IsCanceled)
                 {
                     Firebase.FirebaseException e = task.Exception.Flatten().InnerExceptions[0] as Firebase.FirebaseException;
-                    GetErrorMessage((AuthError)e.ErrorCode);
+                    GetErrorMessageLog((AuthError)e.ErrorCode);
                     return;
                 }
                 if (task.IsFaulted)
                 {// problem detected
                     Firebase.FirebaseException e = task.Exception.Flatten().InnerExceptions[0] as Firebase.FirebaseException;
-                    GetErrorMessage((AuthError)e.ErrorCode);
+                    GetErrorMessageLog((AuthError)e.ErrorCode);
                     return;
                 }
                 if (task.IsCompleted)
                 {
-                    print("User Signed in successfully");
+                    print("User Signed in Successfully");
+                    SIGN_IN_BTN.interactable = false;
                 }
 
             }));
+
+        bridge.SaveData();
+        CleanUpLoginInputs();
+        LoginCompletedTemplateAnime.SetTrigger("success");
+        AppHandler.Instance.CloseSignIn();
+        AppHandler.Instance.OpenMoreFeatures();
     }
 
     public void SignOut()
@@ -41,7 +107,7 @@ public class AuthController : MonoBehaviour
         }
     }
 
-    void GetErrorMessage(AuthError errorCode)
+    void GetErrorMessageReg(AuthError errorCode)
     {
         string msg = "";
         msg = errorCode.ToString();
@@ -50,20 +116,51 @@ public class AuthController : MonoBehaviour
         {
             // existing user
             case AuthError.AccountExistsWithDifferentCredentials:
+                ErrorRegisterMessage_Email.text = msg;
                 break;
-
             // password
             case AuthError.MissingPassword:
-                break;
-            case AuthError.WrongPassword:
+                ErrorRegisterMessage_Password.text = msg;
                 break;
             case AuthError.WeakPassword:
+                ErrorRegisterMessage_Password.text = msg;
+                break;
+            // email
+            case AuthError.InvalidEmail:
+                ErrorRegisterMessage_Email.text = msg;
+                break;
+            case AuthError.MissingEmail:
+                ErrorRegisterMessage_Email.text = msg;
+                break;
+
+            default:
+                break;
+        }
+        print(msg);
+
+    }
+
+    void GetErrorMessageLog(AuthError errorCode)
+    {
+        string msg = "";
+        msg = errorCode.ToString();
+
+        switch (errorCode)
+        {
+            // password
+            case AuthError.MissingPassword:
+                ErrorLoginMessage_Password.text = msg;
+                break;
+            case AuthError.WrongPassword:
+                ErrorLoginMessage_Password.text = msg;
                 break;
 
             // email
             case AuthError.InvalidEmail:
+                ErrorLoginMessage_Email.text = msg;
                 break;
             case AuthError.MissingEmail:
+                ErrorLoginMessage_Email.text = msg;
                 break;
 
             default:
@@ -72,37 +169,136 @@ public class AuthController : MonoBehaviour
         print(msg);
     }
 
-    public void SignUp()
+    public void CheckForRegisterErrors()
     {
-
-        if(emailInput.text == "" || passwordInput.text == "")
+        if ((emailInput.text == ""))
         {
-            print("Please enter both email & password to continue..");
+            print("Please enter both email to continue..");
+            ErrorRegisterMessage_Email.text = Constants.ERROR_REGISTER_EMAIL;
             return;
         }
-            FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(emailInput.text, passwordInput.text)
+        if ((passwordInput.text == ""))
+        {
+            print("Please enter password to continue..");
+            ErrorRegisterMessage_Password.text = Constants.ERROR_REGISTER_PASSWORD;
+            return;
+        }
+        if (!(passwordInput.text.Equals(confirm_passwordInput.text)))
+        {
+            print("Password mismatch");
+            ErrorRegisterMessage_PasswordConfirmation.text = Constants.ERROR_REGISTER_CONFIRM_PASSWORD;
+            return;
+        }
+        if (!(agreement1.isOn))
+        {
+            print("User didnt agree with terms");
+            ErrorRegisterMessage_ErrorMsgTerms.text = Constants.ERROR_REGISTER_TOGGLE;
+            return;
+        }
+    }
+
+    public void CheckForLoginErrors()
+    {
+        if ((log_in_emailInput.text == ""))
+        {
+            print("Please enter both email to continue..");
+            ErrorLoginMessage_Email.text = Constants.ERROR_LOGIN_EMAIL;
+            return;
+        }
+        if ((log_in_passwordInput.text == ""))
+        {
+            print("Please enter password to continue..");
+            ErrorLoginMessage_Password.text = Constants.ERROR_LOGIN_PASSWORD;
+            return;
+        }
+    }
+
+    public void CleanUpRegisterInputs()
+    {
+        Debug.Log("Cleaning..");
+        emailInput.text = "";
+        passwordInput.text = "";
+        confirm_passwordInput.text = "";
+
+
+        ErrorRegisterMessage_Email.text = "";
+        ErrorRegisterMessage_Password.text = "";
+        ErrorRegisterMessage_PasswordConfirmation.text = "";
+        ErrorRegisterMessage_ErrorMsgTerms.text = "";
+
+
+        agreement1.isOn = false;
+        agreement2.isOn = false;
+        Debug.Log("Cleaned..");
+    }
+
+    public void CleanUpLoginInputs()
+    {
+        log_in_emailInput.text = "";
+        log_in_passwordInput.text = "";
+
+        ErrorLoginMessage_Email.text = "";
+        ErrorLoginMessage_Password.text = "";
+    }
+
+    public void SignUp()
+    {
+        if ((emailInput.text == ""))
+        {
+            print("Please enter both email to continue..");
+            ErrorRegisterMessage_Email.text = Constants.ERROR_REGISTER_EMAIL;
+            return;
+        }
+        if ((passwordInput.text == ""))
+        {
+            print("Please enter password to continue..");
+            ErrorRegisterMessage_Password.text = Constants.ERROR_REGISTER_PASSWORD;
+            return;
+        }
+        if (!(passwordInput.text.Equals(confirm_passwordInput.text)))
+        {
+            print("Password mismatch");
+            ErrorRegisterMessage_PasswordConfirmation.text = Constants.ERROR_REGISTER_CONFIRM_PASSWORD;
+            return;
+        }
+        if (!(agreement1.isOn))
+        {
+            print("User didnt agree with terms");
+            ErrorRegisterMessage_ErrorMsgTerms.text = Constants.ERROR_REGISTER_TOGGLE;
+            return;
+        }
+
+        FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(emailInput.text, passwordInput.text)
             .ContinueWith((task =>{
 
                 if (task.IsCanceled)
                 {
                     Firebase.FirebaseException e = task.Exception.Flatten().InnerExceptions[0] as Firebase.FirebaseException;
-                    GetErrorMessage((AuthError)e.ErrorCode);
+                    GetErrorMessageReg((AuthError)e.ErrorCode);
                     return;
                 }
 
                 if (task.IsFaulted)
                 {
                     Firebase.FirebaseException e = task.Exception.Flatten().InnerExceptions[0] as Firebase.FirebaseException;
-                    GetErrorMessage((AuthError)e.ErrorCode);
+                    GetErrorMessageReg((AuthError)e.ErrorCode);
                     return;
                 }
 
                 if (task.IsCompleted)
                 {
                     print("Registration completed successfully! :)");
+                    SIGN_UP_BTN.interactable = false;
                 }
 
             }));
+
+        RegistrationCompletedTemplateAnime.SetTrigger("success");
+        // close register template; open banner Login/SignIn || Login template
+        AppHandler.Instance.CloseCreateAccount();
+        AppHandler.Instance.OpenLoginBanner();
+        // clear text fields
+        CleanUpRegisterInputs();
     }
 
 
@@ -115,13 +311,13 @@ public class AuthController : MonoBehaviour
                 if (task.IsCanceled)
                 {
                     Firebase.FirebaseException e = task.Exception.Flatten().InnerExceptions[0] as Firebase.FirebaseException;
-                    GetErrorMessage((AuthError)e.ErrorCode);
+                    GetErrorMessageLog((AuthError)e.ErrorCode);
                     return;
                 }
                 if (task.IsFaulted)
                 {// problem detected
                     Firebase.FirebaseException e = task.Exception.Flatten().InnerExceptions[0] as Firebase.FirebaseException;
-                    GetErrorMessage((AuthError)e.ErrorCode);
+                    GetErrorMessageLog((AuthError)e.ErrorCode);
                     return;
                 }
                 if (task.IsCompleted)
