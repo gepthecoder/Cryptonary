84c917438e828a489f0e5d3ff53c42f03f7c8ae5
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-09-28 19:09:53 +0200

    Search for Cryptocurrencies + ADD_TRANSACTION button method call

---
+++ Crypto/Assets/Scripts/Search.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Search : MonoBehaviour
+{
+    #region Singleton
+    private static Search instance;
+    public static Search Instance
+    {
+        get
+        {
+            if (instance == null) { instance = FindObjectOfType(typeof(Search)) as Search; }
+            return instance;
+        }
+        set { instance = value; }
+    }
+    #endregion
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public string ID;
+
+    public void ADD_TRANSACTION()
+    {
+        ID = transform.name;
+        Debug.Log("Added transaction: " + ID);
+        TransactionManager.Instance.OpenTransaction(ID, TransactionType.BUY);
+    }
+}
